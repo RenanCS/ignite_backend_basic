8c37d9cd244308537599c80541e6ab6bcc587d46
renan <renan@interprocess.com.br>
2022-02-03 11:34:48 -0300

    chore(auth): clear code

--- src/auth.js
+++ src/auth.js
@@ -3,7 +3,7 @@ const { auth } = require('./config');
 const { createRefreshToken } = require('./database');
 
 function generateJwtAndRefreshToken(email, payload = {}) {
-  const token = jwt.sign(payload, auth.secret ?? "", {
+  const token = jwt.sign(payload, auth.secret, {
     subject: email,
     expiresIn: process.env.NODE_TIME_TO_LIVE,
   });
